806590b1ae9f4fea66dd629df43de814c12c28ec
Nicolo Veneziale <nicoloveneziale@gmail.com>
2024-10-08 22:10:12 +0100

    Added the nav styling and html, implemented the basic js for the nav switching system

---
+++ src/index.js
@@ -0,0 +1,18 @@
+import "./styles.css";
+
+const content = document.querySelector("#content");
+
+document.querySelector("#home").addEventListener("click", () => {
+    content.innerHTML = "";
+    content.appendChild();
+});
+
+document.querySelector("#menu").addEventListener("click", () => {
+    content.innerHTML = "";
+    content.appendChild();
+});
+
+document.querySelector("#about").addEventListener("click", () => {
+    content.innerHTML = "";
+    content.appendChild();
+});
